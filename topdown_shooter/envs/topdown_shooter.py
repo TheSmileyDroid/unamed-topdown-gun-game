78610d80b143c0860d9043fa902a136af17861d7
TheSmileyDroid <ghsilha@gmail.com>
2024-06-13 20:27:42 -0300

    Modelo funcional

--- topdown_shooter/envs/topdown_shooter.py
+++ topdown_shooter/envs/topdown_shooter.py
@@ -15,6 +15,9 @@ class Player:
     angle: float
     health: float
     cooldown: float = 0
+    score: float = 0
+    direction: tuple[int, int] = (0, 0)
+    time_to_next_action: float = 0
 
     def distance(self, other):
         return np.linalg.norm((self.x - other.x, self.y - other.y), 2)
@@ -33,6 +36,7 @@ class Bullet:
     x: float
     y: float
     angle: float
+    real: bool = True
     radius: float = 5
     speed: float = 4
     damage: float = 10
@@ -57,7 +61,9 @@ class TopDownShooterEnv(gym.Env):
 
     def __init__(self, render_mode=None):
         self._agent: Player = Player(x=0, y=0, angle=0, health=100)
-        self._players: list[Player] = [Player(x=0, y=0, angle=0, health=100)] * 7
+        self._players: list[Player] = [
+            Player(x=0, y=0, angle=0, health=100) for _ in range(7)
+        ]
         self._bullets: list[Bullet] = []
         self.window_size = (640, 480)
         self._observation_space = spaces.Dict(
@@ -70,49 +76,47 @@ class TopDownShooterEnv(gym.Env):
                         "health": spaces.Box(low=0, high=100, shape=(1,)),
                     }
                 ),
-                "players": spaces.Sequence(
-                    spaces.Dict(
-                        {
-                            "x": spaces.Box(
-                                low=0, high=self.window_size[0], shape=(1,)
-                            ),
-                            "y": spaces.Box(
-                                low=0, high=self.window_size[1], shape=(1,)
-                            ),
-                            "angle": spaces.Box(low=0, high=2 * np.pi, shape=(1,)),
-                            "health": spaces.Box(low=0, high=100, shape=(1,)),
-                        }
-                    )
+                "players": spaces.Tuple(
+                    [
+                        spaces.Dict(
+                            {
+                                "x": spaces.Box(
+                                    low=0, high=self.window_size[0], shape=(1,)
+                                ),
+                                "y": spaces.Box(
+                                    low=0, high=self.window_size[1], shape=(1,)
+                                ),
+                                "angle": spaces.Box(low=0, high=2 * np.pi, shape=(1,)),
+                                "health": spaces.Box(low=0, high=100, shape=(1,)),
+                            }
+                        )
+                    ]
+                    * 7,
                 ),
-                "near_bullets": spaces.Sequence(
-                    spaces.Dict(
-                        {
-                            "x": spaces.Box(
-                                low=0, high=self.window_size[0], shape=(1,)
-                            ),
-                            "y": spaces.Box(
-                                low=0, high=self.window_size[1], shape=(1,)
-                            ),
-                            "angle": spaces.Box(low=0, high=2 * np.pi, shape=(1,)),
-                        }
-                    ),
+                "near_bullets": spaces.Tuple(
+                    [
+                        spaces.Dict(
+                            {
+                                "x": spaces.Box(
+                                    low=0, high=self.window_size[0], shape=(1,)
+                                ),
+                                "y": spaces.Box(
+                                    low=0, high=self.window_size[1], shape=(1,)
+                                ),
+                                "angle": spaces.Box(low=0, high=2 * np.pi, shape=(1,)),
+                            }
+                        )
+                    ]
+                    * 10,
                 ),
                 "cooldown": spaces.Box(low=0, high=100, shape=(1,)),
             }
         )
-        self.observation_space = self._observation_space
-        self._action_space = spaces.Dict(
-            {
-                "up": spaces.Discrete(2),
-                "down": spaces.Discrete(2),
-                "left": spaces.Discrete(2),
-                "right": spaces.Discrete(2),
-                "shoot": spaces.Discrete(2),
-                "rotate_left": spaces.Discrete(2),
-                "rotate_right": spaces.Discrete(2),
-            }
-        )
-        self.action_space = self._action_space
+        self.observation_space = spaces.flatten_space(self._observation_space)
+
+        self.action_space = spaces.MultiDiscrete(
+            [2, 2, 2, 2, 2, 2, 2]
+        )  # up, down, left, right, shoot, rotate_left, rotate_right
         assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
 
@@ -120,19 +124,29 @@ class TopDownShooterEnv(gym.Env):
         self.clock = None
 
     def _get_near_bullets(self):
-        return sorted(
+        bullets = sorted(
             self._bullets,
             key=lambda b: b.distance(self._agent),
             reverse=True,
         )[:10]
 
+        for i in range(10 - len(bullets)):
+            bullets.append(Bullet(x=0, y=0, angle=0, real=False))
+
+        return bullets
+
     def _get_obs(self):
-        return {
-            "you": self._agent.to_dict(),
-            "players": tuple(map(lambda p: p.to_dict(), self._players)),
-            "near_bullets": tuple(map(lambda b: b.to_dict(), self._get_near_bullets())),
-            "cooldown": self._agent.cooldown,
-        }
+        return spaces.flatten(
+            self._observation_space,
+            {
+                "you": self._agent.to_dict(),
+                "players": tuple(map(lambda p: p.to_dict(), self._players)),
+                "near_bullets": tuple(
+                    map(lambda b: b.to_dict(), self._get_near_bullets())
+                ),
+                "cooldown": self._agent.cooldown,
+            },
+        )
 
     def _get_info(self):
         return {}
@@ -151,15 +165,12 @@ class TopDownShooterEnv(gym.Env):
             health=100,
             cooldown=0,
         )
-        self._players = [
-            Player(
-                x=np.random.randint(0, self.window_size[0]),
-                y=np.random.randint(0, self.window_size[1]),
-                angle=0,
-                health=100,
-                cooldown=0,
-            )
-        ] * 7
+        for player in self._players:
+            player.x = np.random.randint(0, self.window_size[0])
+            player.y = np.random.randint(0, self.window_size[1])
+            player.angle = 0
+            player.health = 100
+            player.cooldown = 0
 
         obs = self._get_obs()
         info = self._get_info()
@@ -169,16 +180,28 @@ class TopDownShooterEnv(gym.Env):
 
         return obs, info
 
-    def _shoot(self):
-        if self._agent.cooldown > 0:
+    @staticmethod
+    def _transform_actions(action):
+        return {
+            "up": action[0],
+            "down": action[1],
+            "left": action[2],
+            "right": action[3],
+            "shoot": action[4],
+            "rotate_left": action[5],
+            "rotate_right": action[6],
+        }
+
+    def _shoot(self, player: Player):
+        if player.cooldown > 0:
             return
-        self._agent.cooldown = 0.5
+        player.cooldown = 0.5
         self._bullets.append(
             Bullet(
-                x=self._agent.x,
-                y=self._agent.y,
-                angle=self._agent.angle,
-                player=self._agent,
+                x=player.x,
+                y=player.y,
+                angle=player.angle,
+                player=player,
             )
         )
 
@@ -206,45 +229,89 @@ class TopDownShooterEnv(gym.Env):
     def _any_players_alive(self):
         return any(p.health > 0 for p in self._players)
 
-    def _update_bullets(self):
-        self._agent.cooldown -= 0.01
+    def _update_players(self):
         for player in self._players:
-            player.cooldown -= 0.01
+            if player.health <= 0:
+                continue
+            player.time_to_next_action -= 0.5
+            if player.time_to_next_action <= 0:
+                player.direction = (
+                    np.random.randint(0, 3) - 1,
+                    np.random.randint(0, 3) - 1,
+                )
+                player.angle = np.random.rand() * 2 * np.pi
+                player.time_to_next_action = np.random.randint(0, 10)
+
+            player.x += player.direction[0]
+            player.y += player.direction[1]
+            player.x = np.clip(player.x, 0, self.window_size[0])
+            player.y = np.clip(player.y, 0, self.window_size[1])
+
+            if np.random.rand() < 0.1:
+                self._shoot(player)
+
+    def _update_bullets(self):
         for bullet in self._bullets:
             bullet.x += np.cos(bullet.angle) * bullet.speed
             bullet.y += np.sin(bullet.angle) * bullet.speed
             if bullet.x < 0 or bullet.x > self.window_size[0]:
                 self._bullets.remove(bullet)
             if bullet.y < 0 or bullet.y > self.window_size[1]:
-                self._bullets.remove(bullet)
+                try:
+                    self._bullets.remove(bullet)
+                except ValueError:
+                    pass
 
             for player in self._players:
+                if player.health <= 0:
+                    continue
                 if bullet.collides_with(player):
                     if bullet.player is not player:
                         player.health -= bullet.damage
-                        self._bullets.remove(bullet)
+                        player.score -= bullet.damage
+                        bullet.player.score += bullet.damage
+                        try:
+                            self._bullets.remove(bullet)
+                        except ValueError:
+                            pass
                         break
 
             if bullet.collides_with(self._agent):
                 if self._agent is not bullet.player:
-                    self._bullets.remove(bullet)
+                    try:
+                        self._bullets.remove(bullet)
+                    except ValueError:
+                        pass
                     self._agent.health -= bullet.damage
+                    self._agent.score -= bullet.damage
+                    bullet.player.score += bullet.damage
 
     def step(
         self, action: ActType
     ) -> tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]:
+        action: dict = self._transform_actions(action)
+
+        self._agent.cooldown -= 0.01
+        self._agent.score = 0
+        for player in self._players:
+            player.cooldown -= 0.01
+            player.score = 0
 
         self._move(action)
 
         if action["shoot"]:
-            self._shoot()
+            self._shoot(self._agent)
 
         self._rotate(action)
 
-        terminated = not self._any_players_alive()
-        reward = 100 if terminated else 0
-
         self._update_bullets()
+
+        self._update_players()
+
+        terminated = not self._any_players_alive()
+        reward = 1000 if terminated else 0
+
+        reward += self._agent.score
 
         obs = self._get_obs()
         info = self._get_info()
@@ -279,6 +346,8 @@ class TopDownShooterEnv(gym.Env):
         )
 
         for player in self._players:
+            if player.health <= 0:
+                continue
             pygame.draw.circle(
                 canvas,
                 (100, 0, 0),
